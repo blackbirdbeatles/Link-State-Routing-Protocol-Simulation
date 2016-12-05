ce4a6c71eee91ee6f6024a1f248d25625d79fc4d
Rethal <grundman@rams.colostate.edu>
2016-12-04 22:20:22 -0700

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -48,41 +48,33 @@ Manager::Manager(ifstream& inFile) {
 
 int Manager::makeRouters() {
 	//creates routers as children branches
-	pid_t routerID;
 	for (int i = 0; i < numberOfRouters; i++) {
 		//fork off routers with their individualized information
-		//if and else will run essentially simultaneously; might be good to fork off
-		//all routers and then do parent code outside of for loop rather than waiting
-		//for each router to complete its instantiation.  That said, not sure - it would
-		//be easier if the routers were their own executable but whatever
 		routerID = fork();
 		if (routerID == 0) {
 			//I'm the child
-			//create router with no information
-			//Router router();
-			//break?  run router methods? idk, but be careful that they don't interfere with main.
-			//in fact might want routerID to be a Manager global variable since routers will also
-			//be part-manager (just not the parent part) I think
-			//this could be done by essentially keeping the routers in an infinite loop though,
-			//and when they break it they exit.  
-		}
-		else {
-			//I'm the parent
-			//make specific routing table to provide to router when it asks for its neighbors
-			//since 
+			//create router with information from i
 			vector<vector<int>> individualRoutingTable;
 			for (unsigned int j = 0; j < routingTable.size(); j++) {
 				if (routingTable[j][0] == i || routingTable[j][1] == i) {
 					individualRoutingTable.push_back(routingTable[j]);
 				}
 			}
-			//send routing table and other information to the router (this could be done in main or simply
-			//after all routers are created (after the for loop), if we create a vector of the individualRoutingTables
-			//which would allow us to essentially create all of the threads and deal with them simultaneously)
-			//we'd need to keep track of which thread is which probably
-			//wpid = wait(&status); (code from previous class that could potentially help)
+			//Router router(individualRoutingTable, numberOfRouters);
+			//break?  run router methods? idk
+		}
+		else {
+			//I'm the parent
+			//wpid = wait(&status);
 		}
 	}
+}
+
+int Manager::managerPrint(string message) {
+	ofstream manaFile;
+	manaFile.open("manager.out");
+	manaFile << message << endl;
+	return 0;
 }
 
 
